4cc78a1d477d373c7dc00b6ca508d653ae539d7e
hylinktree <hylinktree@aa.com>
2021-04-29 16:15:23 +0800

    nocomment

--- mk.sh
+++ mk.sh
@@ -1,5 +1,5 @@
 dotnet publish -r win-x64 -c Release --self-contained
-rm -rf csrunner
 mkdir csrunner
 cp -r bin/Release/net5.0/win-x64/* csrunner/
 tar zcvf csrunner.tgz csrunner/
+rm -rf csrunner
