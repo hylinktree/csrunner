d660d0252519c4a5102c462a202d3effd41b751f
hylinktree <hylinktree@aa.com>
2021-05-05 18:52:37 +0800

    nocomment

--- mk.sh
+++ mk.sh
@@ -1,5 +1,5 @@
-dotnet publish -r win-x64 -c Release --self-contained
+dotnet publish -r win-x64 -c Release -p:PublishSingleFile=true --self-contained true
 mkdir csrunner
-cp -r bin/Release/net5.0/win-x64/* csrunner/
+cp -r bin/Release/net5.0/win-x64/publish/* csrunner/
 tar zcvf csrunner.tgz csrunner/
 rm -rf csrunner
